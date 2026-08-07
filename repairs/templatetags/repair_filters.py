01629181bebe74f4508e9c915e67a65d99fc8c1e
Mirikrom <tva730818@gmail.com>
2026-08-07 10:19:43 +0500

    telefon nomerni olish

--- repairs/templatetags/repair_filters.py
+++ repairs/templatetags/repair_filters.py
@@ -15,6 +15,23 @@ def phone_without_prefix(value):
 
 
 @register.filter
+def phone_tel_href(value):
+    """Dialer uchun tel: havola. +998901234567 yoki 901234567 -> tel:+998901234567"""
+    if not value:
+        return ''
+    digits = ''.join(ch for ch in str(value) if ch.isdigit())
+    if not digits:
+        return ''
+    if digits.startswith('998') and len(digits) >= 12:
+        return f'tel:+{digits}'
+    if len(digits) == 9:
+        return f'tel:+998{digits}'
+    if digits.startswith('0') and len(digits) == 10:
+        return f'tel:+998{digits[1:]}'
+    return f'tel:+{digits}'
+
+
+@register.filter
 def intcomma_uz(value):
     """Raqamni vergul bilan ko'rsatadi: 200000 -> 200,000, 20000 -> 20,000"""
     if value is None:
